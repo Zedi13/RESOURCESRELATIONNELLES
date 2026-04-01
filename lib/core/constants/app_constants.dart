caa0760dbafecaf7052465063eb9ae7eb867a25c
Zedi13 <chatainflorian1@gmail.com>
2026-04-01 10:44:23 +0200

    form connection mobile

--- lib/core/constants/app_constants.dart
+++
@@ -1,5 +0,0 @@
-class AppConstants {
-  static const String appName = '(RE)Sources Relationnelles';
-  static const String appTagline = 'La plateforme pour améliorer vos relations';
-  static const String appVersion = '1.0.0';
-}
